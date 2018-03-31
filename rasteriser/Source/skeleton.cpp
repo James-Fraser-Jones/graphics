4042753be10cd3fa13ffa5c2e4cd6e82e12ff535
Nathan <nd14127@my.bristol.ac.uk>
2018-03-31 14:22:03 +0100

    back in a box shape but still broken

--- rasteriser/Source/skeleton.cpp
+++ rasteriser/Source/skeleton.cpp
@@ -40,42 +40,40 @@ void Draw(screen* screen, const vector <Triangle>& triangles);
 void InterpolatePixel( Pixel a, Pixel b, vector<Pixel>& result );
 void TransformationMatrix(mat4& M, vec4 pos, vec4 rot){
 
-  mat4 toOrigin (1,0,0,-pos.x,
-                 0,1,0,-pos.y,
-                 0,0,1,-pos.z,
-                 0,0,0,1);
+    mat4 toOrigin (1,0,0,-pos.x,
+                    0,1,0,-pos.y,
+                    0,0,1,-pos.z,
+                    0,0,0,1);
 
-  /* //don't seem to need this at the moment
-  mat4 toCamera (1,0,0,pos.x,
-                 0,1,0,pos.y,
-                 0,0,1,pos.z,
-                 0,0,0,1);
-  //*/
+    /* //don't seem to need this at the moment
+    mat4 toCamera (1,0,0,pos.x,
+                    0,1,0,pos.y,
+                    0,0,1,pos.z,
+                    0,0,0,1);
+    //*/
 
-  mat4 rotationX(1,0,0,0,
-                 0,cos(rot.x),-sin(rot.x),0,
-                 0,sin(rot.x),cos(rot.x),0,
-                 0,0,0,1);
+    mat4 rotationX(1,0,0,0,
+                    0,cos(rot.x),-sin(rot.x),0,
+                    0,sin(rot.x),cos(rot.x),0,
+                    0,0,0,1);
 
-  mat4 rotationY(cos(rot.y),0,sin(rot.y),0,
-                 0,1,0,0,
-                 -sin(rot.y),0,cos(rot.y),0,
-                 0,0,0,1);
+    mat4 rotationY(cos(rot.y),0,sin(rot.y),0,
+                    0,1,0,0,
+                    -sin(rot.y),0,cos(rot.y),0,
+                    0,0,0,1);
 
-  mat4 rotationZ(cos(rot.z),-sin(rot.z),0,0,
-                 sin(rot.z),cos(rot.z),0,0,
-                 0,0,1,0,
-                 0,0,0,1);
+    mat4 rotationZ(cos(rot.z),-sin(rot.z),0,0,
+                    sin(rot.z),cos(rot.z),0,0,
+                    0,0,1,0,
+                    0,0,0,1);
 
-  M = toOrigin*(rotationZ*rotationY*rotationX);
+    M = toOrigin*(rotationZ*rotationY*rotationX);
 }
 
 void VertexShader(const vec4& v, Pixel& p){
     mat4 M;
     TransformationMatrix(M, cameraPos, cameraRot);
-
     vec4 localV = v*M; //for whatever reason they have to multiply this way around
-
     p.x = (focalLength * (localV.x/localV.z) + (SCREEN_WIDTH/2));
     p.y = (focalLength * (localV.y/localV.z) + (SCREEN_HEIGHT/2));
 }
@@ -83,16 +81,14 @@ void VertexShader(const vec4& v, Pixel& p){
 void InterpolatePixel( Pixel a, Pixel b, vector<Pixel>& result ) {
     int N = result.size(); //we have to know the size in advance
     Pixel step;
-    step.x = (a.x - b.x) / float(max(N-1,1));
-    step.y = (a.y - b.y) / float(max(N-1,1));
-    step.z = (a.z - b.z) / float(max(N-1,1));
+    step.x = (b.x - a.x) / float(max(N-1,1));
+    step.y = (b.y - a.y) / float(max(N-1,1));
+    step.z = (b.z - a.z) / float(max(N-1,1));
     Pixel current = a;
     for(int i=0; i<N; i++){
         result[i] = current;
         current.x = current.x + step.x;
-        current.x = current.x + step.x;
-        current.x = current.x + step.x;
-
+        current.y = current.y + step.y;
     }
 }
 
@@ -130,8 +126,6 @@ void ComputePolygonRows(const vector<Pixel>& vertexPixels, vector<Pixel>& leftPi
     for(int x = 0; x < ROWS; x++) {
         cout << leftPixels[x].y << "\n";
     }
-
-
     for (int q = 0; q < 3; q++){
         int p = q + 1;
         if (q > 1) {p = 0;}
@@ -154,50 +148,32 @@ void ComputePolygonRows(const vector<Pixel>& vertexPixels, vector<Pixel>& leftPi
             }
         }
     }
-
-    //TODO:assign left and right pixels vars to actual values on triangle
 }
 
-void DrawLineSDL(screen* screen, ivec2 a, ivec2 b, vec3 color) {
-  ivec2 delta = glm::abs(a - b);
-  int pixels = glm::max(delta.x, delta.y) + 1;
-  vector<ivec2> line(pixels);
-  Interpolate(a, b, line);
+void DrawLineSDL(screen* screen, Pixel a, Pixel b, vec3 color) {
+    Pixel deltaY;
+    deltaY.y = glm::abs(a.y - b.y);
+    Pixel deltaX;
+    deltaX.x = glm::abs(a.x - b.x);
 
-  for (int i = 0; i < pixels; i++){
-    if ((line[i].x >= 0) && (line[i].x < SCREEN_WIDTH) && (line[i].y >= 0) && (line[i].y < SCREEN_HEIGHT)) {
-      PutPixelSDL(screen, line[i].x, line[i].y, color);
+    int pixels = glm::max(deltaX.x, deltaY.y) + 1;
+    vector<Pixel> line(pixels);
+    InterpolatePixel(a, b, line);
+
+    for (int i = 0; i < pixels; i++){
+        if ((line[i].x >= 0) && (line[i].x < SCREEN_WIDTH) && (line[i].y >= 0) && (line[i].y < SCREEN_HEIGHT)) {
+        PutPixelSDL(screen, line[i].x, line[i].y, color);
+        }
     }
-  }
-}
-
-int main( int argc, char* argv[] ){
-
-
-  screen *screen = InitializeSDL( SCREEN_WIDTH, SCREEN_HEIGHT, FULLSCREEN_MODE );
-
-  vector<Triangle> triangles;
-  LoadTestModel(triangles);
-
-  while( NoQuitMessageSDL() ){
-    Update();
-    Draw(screen, triangles);
-    SDL_Renderframe(screen);
-  }
-
-  SDL_SaveImage(screen, "screenshot.bmp");
-
-  KillSDL(screen);
-  return 0;
 }
 
 void DrawPolygonEdges( const vector<vec4>& vertices, screen* screen )
 {
     int V = vertices.size();
     // Transform each vertex from 3D world position to 2D image position:
-    vector<ivec2> projectedVertices(V);
+    vector<Pixel> projectedVertices(V);
     for(int i=0; i<V; ++i) {
-        //VertexShader(vertices[i], projectedVertices[i]);
+        VertexShader(vertices[i], projectedVertices[i]);
     }
     // Loop over all vertices and draw the edge from it to the next vertex:
     for(int i=0; i<V; ++i) {
@@ -206,8 +182,8 @@ void DrawPolygonEdges( const vector<vec4>& vertices, screen* screen )
         DrawLineSDL(screen, projectedVertices[i], projectedVertices[j], color);
     }
 }
-void DrawRows(const vector<Pixel>& leftPixels, const vector<Pixel>& rightPixels, screen * screen, vec3 color) {
 
+void DrawRows(const vector<Pixel>& leftPixels, const vector<Pixel>& rightPixels, screen * screen, vec3 color) {
     for(int j = 0; j < leftPixels.size(); j++) {
         for(int i = leftPixels[j].x; i <= rightPixels[j].x; i++) {
             PutPixelSDL(screen, i, leftPixels[j].y, color);
@@ -215,6 +191,7 @@ void DrawRows(const vector<Pixel>& leftPixels, const vector<Pixel>& rightPixels,
     }
 
 }
+
 void DrawPolygon( const vector<vec4>& vertices, screen* screen, vec3 color)
 {
     int V = vertices.size();
@@ -237,7 +214,7 @@ void Draw(screen* screen, const vector <Triangle>& triangles){
         vertices[0] = triangles[i].v0;
         vertices[1] = triangles[i].v1;
         vertices[2] = triangles[i].v2;
-        //DrawPolygonEdges(vertices, screen);
+        DrawPolygonEdges(vertices, screen);
         DrawPolygon(vertices, screen, triangles[i].color);
     }
 
@@ -245,62 +222,76 @@ void Draw(screen* screen, const vector <Triangle>& triangles){
 
 /*Place updates of parameters here*/
 void Update(){
-  /* //Compute frame time
-  static int t = SDL_GetTicks();
-  int t2 = SDL_GetTicks();
-  float dt = float(t2-t);
-  t = t2;
-  std::cout << "Render time: " << dt << " ms." << std::endl;
-  //*/
+    /* //Compute frame time
+    static int t = SDL_GetTicks();
+    int t2 = SDL_GetTicks();
+    float dt = float(t2-t);
+    t = t2;
+    std::cout << "Render time: " << dt << " ms." << std::endl;
+    //*/
 
-  float lookSpeed = 0.02;
-  float moveSpeed = 0.02;
+    float lookSpeed = 0.02;
+    float moveSpeed = 0.02;
 
-  //Collect button inputs
-  vec4 lookVector(0, 0, 0, 1);
-  vec4 moveVector(0, 0, 0, 1);
-  const uint8_t* keystate = SDL_GetKeyboardState( 0 );
-  if(keystate[SDL_SCANCODE_LEFT]){
+    //Collect button inputs
+    vec4 lookVector(0, 0, 0, 1);
+    vec4 moveVector(0, 0, 0, 1);
+    const uint8_t* keystate = SDL_GetKeyboardState( 0 );
+    if(keystate[SDL_SCANCODE_LEFT]){
     lookVector.y = -1;
-  }
-  else if(keystate[SDL_SCANCODE_RIGHT]){
+    }
+    else if(keystate[SDL_SCANCODE_RIGHT]){
     lookVector.y = 1;
-  }
-  if(keystate[SDL_SCANCODE_UP]){
+    }
+    if(keystate[SDL_SCANCODE_UP]){
     lookVector.x = 1;
-  }
-  else if(keystate[SDL_SCANCODE_DOWN]){
+    }
+    else if(keystate[SDL_SCANCODE_DOWN]){
     lookVector.x = -1;
-  }
-  if(keystate[SDL_SCANCODE_W]){
+    }
+    if(keystate[SDL_SCANCODE_W]){
     moveVector.z = -1;
-  }
-  else if(keystate[SDL_SCANCODE_S]){
+    }
+    else if(keystate[SDL_SCANCODE_S]){
     moveVector.z = 1;
-  }
-  if(keystate[SDL_SCANCODE_A]){
+    }
+    if(keystate[SDL_SCANCODE_A]){
     moveVector.x = 1;
-  }
-  else if(keystate[SDL_SCANCODE_D]){
+    }
+    else if(keystate[SDL_SCANCODE_D]){
     moveVector.x = -1;
-  }
-  if(keystate[SDL_SCANCODE_SPACE]){
+    }
+    if(keystate[SDL_SCANCODE_SPACE]){
     moveVector.y = 1;
-  }
-  else if(keystate[SDL_SCANCODE_LCTRL]){
+    }
+    else if(keystate[SDL_SCANCODE_LCTRL]){
     moveVector.y = -1;
-  }
+    }
 
-  //*
-  //Modify global variables
-  mat4 rotate, rotation, translate;
+    //*
+    //Modify global variables
+    mat4 rotate, rotation, translate;
 
-  TransformationMatrix(rotate, lookVector*lookSpeed, vec4(0,0,0,0)); //translation of vector which stores camera rotation values
-  TransformationMatrix(rotation, vec4(0,0,0,0), cameraRot*rotate); //rotation by new camera rotation values
-  TransformationMatrix(translate, rotation*moveVector*moveSpeed, vec4(0,0,0,0)); //translation of vector which stores camera position values
+    TransformationMatrix(rotate, lookVector*lookSpeed, vec4(0,0,0,0)); //translation of vector which stores camera rotation values
+    TransformationMatrix(rotation, vec4(0,0,0,0), cameraRot*rotate); //rotation by new camera rotation values
+    TransformationMatrix(translate, rotation*moveVector*moveSpeed, vec4(0,0,0,0)); //translation of vector which stores camera position values
 
-  cameraRot = cameraRot*rotate;
-  cameraPos = cameraPos*translate;
-  //*/
+    cameraRot = cameraRot*rotate;
+    cameraPos = cameraPos*translate;
+    //*/
+}
 
+int main( int argc, char* argv[] ) {
+    screen *screen = InitializeSDL( SCREEN_WIDTH, SCREEN_HEIGHT, FULLSCREEN_MODE );
+    vector<Triangle> triangles;
+    LoadTestModel(triangles);
+
+    while( NoQuitMessageSDL() ){
+        Update();
+        Draw(screen, triangles);
+        SDL_Renderframe(screen);
+    }
+    SDL_SaveImage(screen, "screenshot.bmp");
+    KillSDL(screen);
+    return 0;
 }
